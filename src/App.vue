4c1b121bddfcf428612d214fa8ffb6df0710601a
Berke Batur <aberkebatur@gmail.com>
2021-03-02 21:46:24 +0300

    added project

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container py-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-12">
+        <svg class="mb-5" xmlns="http://www.w3.org/2000/svg" width="279.3" height="100.533" viewBox="0 0 279.3 100.533">
+        <g id="katman_2" data-name="katman 2" transform="matrix(1, 0.017, -0.017, 1, 1.644, 0)">
+          <g id="Layer_1" data-name="Layer 1">
+            <path id="Path_10" data-name="Path 10" d="M16.174,5.288l9.368,5.506a7.473,7.473,0,0,1,3.933,6.382V46.853a8.009,8.009,0,0,0,2.539,5.7l9.761,9.815a15.375,15.375,0,0,1,4.988,10.941,15.375,15.375,0,0,1-4.988,10.941l-9.046,8.939a9.386,9.386,0,0,1-6.579,2.592H18.105l9.725-9.636a17.717,17.717,0,0,0,0-25.708l-9.868-9.779A5.47,5.47,0,0,1,16.174,46.8V5.288M14.834,3V46.818a6.776,6.776,0,0,0,2.127,4.809l9.868,9.779a16.376,16.376,0,0,1,0,23.795L14.78,97.143H26.043a10.727,10.727,0,0,0,7.58-2.968l9.028-8.939c7.151-7.151,7.151-16.662,0-23.795l-9.743-9.815a6.811,6.811,0,0,1-2.145-4.773V17.177a8.939,8.939,0,0,0-4.541-7.544Z" transform="translate(11.643 2.363)" fill="#fff"/>
+            <path id="Path_11" data-name="Path 11" d="M27.817,58.477l-9.761-9.8a6.793,6.793,0,0,1-2.127-4.755V14.3A8.939,8.939,0,0,0,11.37,6.7L0,0V43.889A6.829,6.829,0,0,0,2.181,48.7l9.868,9.779a16.376,16.376,0,0,1,0,23.759L0,94.214H11.245a10.726,10.726,0,0,0,7.544-3.039l9.028-8.939C35.022,75.085,35.022,65.61,27.817,58.477Z" fill="#fff"/>
+            <path id="Path_12" data-name="Path 12" d="M42.953,38.15c3.468,0,5.256,1.287,5.256,3.844a3.343,3.343,0,0,1-2.181,3.218,3.343,3.343,0,0,1,2.646,3.575c0,2.217-1.412,4.237-5.256,4.237H38.18V38.15Zm-2.27,6.06H43.1c1.788,0,2.61-.84,2.61-2.056s-.572-1.788-2.932-1.788H40.683Zm0,6.6h2.539c1.984,0,2.914-.8,2.914-2.235s-1.037-2.217-3.218-2.217H40.683Z" transform="translate(30.076 30.053)" fill="#fff"/>
+            <path id="Path_13" data-name="Path 13" d="M59.994,38.15v2.181H53.415v3.9H59.35v2.145H53.415v4.434h6.579v2.145H50.93V38.15Z" transform="translate(40.12 30.053)" fill="#fff"/>
+            <path id="Path_14" data-name="Path 14" d="M71.056,52.953,68.2,46.946h-2.86v6.007H62.85V38.15h5.024c3.415,0,5.363,1.484,5.363,4.434a3.844,3.844,0,0,1-2.682,3.951l3.289,6.418ZM67.713,44.8c2.092,0,3.129-.554,3.129-2.217s-1.037-2.253-3.129-2.253H65.335V44.8Z" transform="translate(49.51 30.053)" fill="#fff"/>
+            <path id="Path_15" data-name="Path 15" d="M84.657,52.953l-4.291-6.758L78.185,49v3.951H75.7V38.15h2.485V45.3l5.363-7.151h3.057l-4.684,5.971,5.649,8.831Z" transform="translate(59.632 30.053)" fill="#fff"/>
+            <path id="Path_16" data-name="Path 16" d="M98,38.15v2.181H91.4v3.9H97.34v2.145H91.4v4.434H98v2.145H88.92V38.15Z" transform="translate(70.047 30.053)" fill="#fff"/>
+            <path id="Path_17" data-name="Path 17" d="M105.315,38.15c3.307,0,4.952,1.323,4.952,3.719a3.415,3.415,0,0,1-2.664,3.361,3.576,3.576,0,0,1,3.129,3.719c0,1.9-1.305,4-4.881,4H101.06V38.15Zm-3.575,6.776h4.04c2.735,0,3.879-1.448,3.879-3.057s-.876-3.075-4.308-3.075h-3.576Zm0,7.383h4.148c2.9,0,4.237-1.5,4.237-3.361,0-2.2-1.52-3.379-4.684-3.379h-3.7Z" transform="translate(79.61 30.053)" fill="#fff"/>
+            <path id="Path_18" data-name="Path 18" d="M112.78,52.953l4.97-14.8h.59l4.988,14.8h-.7l-1.287-3.808h-6.6l-1.287,3.808Zm2.181-4.451h6.168l-3.075-9.171Z" transform="translate(88.842 30.053)" fill="#fff"/>
+            <path id="Path_19" data-name="Path 19" d="M134.5,38.15v.644h-4.881V52.953h-.661V38.794H124.08V38.15Z" transform="translate(97.744 30.053)" fill="#fff"/>
+            <path id="Path_20" data-name="Path 20" d="M136.75,46.91V38.15h.644v8.76c0,3.575,1.591,5.524,4.505,5.524S146.4,50.5,146.4,46.91V38.15h.644v8.76c0,4.344-2,6.168-5.149,6.168S136.75,51.254,136.75,46.91Z" transform="translate(107.725 30.053)" fill="#fff"/>
+            <path id="Path_21" data-name="Path 21" d="M159.674,52.953,156.1,46.535a7.58,7.58,0,0,1-1.323,0h-3.862v6.329h-.644V38.15h4.505c3.289,0,5.167,1.484,5.167,4.255a3.826,3.826,0,0,1-3,4l3.576,6.543Zm-4.827-6.972c2.7,0,4.523-.983,4.523-3.575s-1.788-3.575-4.523-3.575h-3.862V45.98Z" transform="translate(118.375 30.053)" fill="#fff"/>
+          </g>
+        </g>
+      </svg>
+
+        <h3 class="mb-4 bb-title">To Do App</h3>
+        <div class="todos">
+          <to-do :token="token"></to-do>
+        </div>
+      </div>
+    </div>
+
+    <!--<button @click="getToken()" class="btn btn-danger">Token Oluştur</button> --> 
+    
+  </div>
+</template>
+
+<script>
+import toDo from "./components/toDo";
+
+export default {
+  name: "app",
+  components: {
+    "to-do": toDo
+  },
+  data() {
+    return {
+      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkJlcmtlIEJhdHVyIiwiaWQiOjUxLCJpYXQiOjE2MTQ2ODcxMzIsImV4cCI6MTYxNDc1OTEzMn0.owWYLqoYLbM-KosA-nl5DtGg_SWD7dFTfd9bicqAZ2o",
+    };
+  }
+};
+</script>
+<style lang="scss">
+body {
+  font-family: "Rubik", sans-serif;
+  background-image: url("https://berkebatur.com/img/back-min.jpg");
+}
+
+.bb-title {
+  font-size: 35px;
+  text-transform: uppercase;
+  color: #fff;
+}
+</style>
---
+++ src/components/toDo.vue
@@ -0,0 +1,224 @@
+<template>
+  <div>
+    <div>
+      <div class="form-group">
+        <input
+          class="form-control bb-input"
+          type="text"
+          placeholder="Note something.."
+          name="todoInput"
+          v-model="todoInput"
+        />
+      </div>
+      <div class="form-group">
+        <button class="form-control" id="todo-btn" @click="addToDo()">
+          <span class="ml-2">Add</span>
+        </button>
+      </div>
+    </div>
+
+    <div>
+      <h6 class="mb-3 alt-title">To Do's:</h6>
+      <ul class="to-do-list-ul">
+        <li v-for="(todo, index) in todolists.body" :key="todo.id">
+          <span :title="todo.name" class="todo-name">{{ todo.name }}</span>
+          <button
+            @click="deleteTodo(todo.id, index)"
+            class="delete-btn d-flex justify-content-center align-items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="14.922"
+              viewBox="0 0 15 14.922"
+            >
+              <path
+                id="close"
+                d="M16.475,15.419l6.18-6.18A.747.747,0,0,0,21.6,8.183l-6.18,6.18L9.208,8.214a.68.68,0,0,0-1.025,0,.74.74,0,0,0,0,1.056l6.18,6.149L8.183,21.6a.715.715,0,0,0,0,1.056.81.81,0,0,0,.528.217.681.681,0,0,0,.528-.217l6.211-6.18,6.211,6.149a.964.964,0,0,0,.528.217.779.779,0,0,0,.528-.217.715.715,0,0,0,0-1.056Z"
+                transform="translate(-7.95 -7.95)"
+                fill="#222327"
+              />
+            </svg>
+          </button>
+          <button @click="deneme(todo.id, index)">Deneme</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  props: ["token"],
+  data() {
+    return {
+      todolists: {
+        body: [
+          {
+            name: "",
+          },
+        ],
+      },
+      error: null,
+      todoInput: null,
+    };
+  },
+  methods: {
+    getTodoList() {
+      axios
+        .get("https://aodapi.eralpsoftware.net/todo", {
+          headers: {
+            token: this.token,
+          },
+        })
+
+        .then((response) => (this.todolists = response.data));
+    },
+    deleteTodo(id, index) {
+      axios.delete("https://aodapi.eralpsoftware.net/todo/" + id, {
+        headers: {
+          token: this.token,
+        },
+      });
+
+      this.$delete(this.todolists.body, index);
+    }, 
+    addToDo() {
+      axios.post(
+        "https://aodapi.eralpsoftware.net/todo",
+        {
+          name: this.todoInput,
+        },
+        {
+          headers: {
+            token: this.token,
+          },
+        }
+      );
+      const newToDo = {
+        name: this.todoInput,
+      };
+      this.todolists.body.push(newToDo);
+    },
+    deneme(id,index) {
+      alert("Id:" + id + "  Index:" + index )
+    }
+  },
+  mounted: function () {
+    this.getTodoList();
+  },
+};
+</script>
+
+
+<style lang="scss">
+.bb-input {
+  border-radius: 0px;
+  border: 2px solid #fff;
+  background: transparent;
+  color: #fff !important;
+  &:focus {
+    border: 2px solid #ffcb04;
+    background: transparent;
+  }
+}
+#todo-btn {
+  cursor: pointer;
+
+  color: #222327;
+  font-weight: 600;
+  border-radius: 0px;
+  background: transparent;
+  position: relative;
+  transition: 600ms all;
+  overflow: hidden;
+  border: 2px solid white;
+  &:hover {
+    color: #fff;
+    &::after {
+      bottom: -100%;
+    }
+  }
+
+  &::after {
+    content: "";
+    background-color: #fff;
+    width: 100%;
+    height: 100%;
+    transition: 300ms all;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+}
+
+.to-do-list-ul {
+  list-style-type: none;
+  padding: 0px;
+  margin: 0px;
+
+  .done {
+    span {
+      &:after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 3px;
+        background-color: #5cb85c;
+      }
+      text-decoration: line-through;
+    }
+  }
+
+  .do {
+    span {
+      &:after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 3px;
+        background-color: #cf2c04;
+      }
+    }
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    padding: 6px 8px;
+    font-weight: 500;
+    background-color: darken(#fff, 6%);
+    margin-bottom: 10px;
+    &:nth-last-child(1) {
+      margin-bottom: 0px;
+    }
+    &:hover {
+      background-color: darken(#fff, 10%);
+    }
+  }
+}
+
+.todo-name {
+  width: 65%;
+  overflow: hidden; /* taşanları gizle */
+  white-space: nowrap; /* alt satıra hiç inme */
+  text-overflow: ellipsis;
+}
+
+.delete-btn {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+.alt-title {
+  color: #fff;
+}
+</style>
